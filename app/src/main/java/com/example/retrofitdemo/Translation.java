e01355f0e662224f5aab096fb30d6a7f3216c8e4
Yang <1007679727@qq.com>
2021-01-14 17:26:21 +0800

    临时更改 demo文件

---
+++ app/src/main/java/com/example/retrofitdemo/Translation.java
@@ -0,0 +1,35 @@
+package com.example.retrofitdemo;
+
+
+import android.util.Log;
+import com.example.retrofitutil.ResponseBean;
+
+public class Translation implements ResponseBean {
+    private int sid;
+    private String tts;
+    private String content;
+    private String note;
+    private int love;
+    private String translation;
+    private String picture;
+    private String picture2;
+    private String caption;
+    private String dataline;
+    private int s_pv;
+    private int SP_pv;
+    private String[] tags;
+    private String fenxiang_img;
+
+
+
+
+    //定义 输出返回数据 的方法
+    public void show() {
+        Log.e("TAG", "Rxjava翻译结果：" + sid);
+        Log.e("TAG", "Rxjava翻译结果：" + tts);
+        Log.e("TAG", "Rxjava翻译结果：" + content);
+        Log.e("TAG", "Rxjava翻译结果：" + note);
+        Log.e("TAG", "Rxjava翻译结果：" + love);
+        Log.e("TAG", "Rxjava翻译结果：" + translation);
+    }
+}
