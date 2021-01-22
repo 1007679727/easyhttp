0202111869402d75e19be5d1e4db062b9d30d0fd
Yang <1007679727@qq.com>
2021-01-22 15:17:20 +0800

    ResponseBean给定默认code和message

--- retrofitutil/src/main/java/com/example/retrofitutil/datainterface/ResponseBean.java
+++ retrofitutil/src/main/java/com/example/retrofitutil/datainterface/ResponseBean.java
@@ -3,6 +3,17 @@ package com.example.retrofitutil.datainterface;
 import org.json.JSONObject;
 
 public class ResponseBean {
+    private int code;
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
     /**
      * 提供一个默认的实现，没有任何参数和方法
      * 此处不提供默认返回值的原因是retrofit创建call对象的时候会遍历父类和子类的所有变量
